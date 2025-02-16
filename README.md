0a982d542a511f6e6df3a3f28b6e8e7856a17a60
hakim_jazuli <muhammadhakimjazuli@gmail.com>
2025-02-16 16:37:25 +0700

    ok

--- README.md
+++ README.md
@@ -12,8 +12,13 @@ it is important to use `npm`, so you can get starter project;
 - `neinthImport_` can be used inside neinth instance declaration for typehintin of `neinth` managed imports;
 
 ## how to use
-- refer to [neinthManage](#neinthmanage) for configuration;
-- refer to [neinth](#neinth) for file handlers;
+- refer to [neinthManage](#neinthmanage) for `configuration`;
+- refer to [neinth](#neinth) for `file handlers`;
+- when everything is ready, you can run on your terminal:
+
+```shell
+neinth
+```
 
 ## exported-api
 - [neinth](#neinth)
--- index.mjs
+++ index.mjs
@@ -19,8 +19,13 @@
  * - `neinthImport_` can be used inside neinth instance declaration for typehintin of `neinth` managed imports;
  * 
  * ## how to use
- * - refer to [neinthManage](#neinthmanage) for configuration;
- * - refer to [neinth](#neinth) for file handlers;
+ * - refer to [neinthManage](#neinthmanage) for `configuration`;
+ * - refer to [neinth](#neinth) for `file handlers`;
+ * - when everything is ready, you can run on your terminal:
+ * 
+ * ```shell
+ * neinth
+ * ```
  * 
  */
 export { neinth } from './src/main/neinth.export.mjs';
